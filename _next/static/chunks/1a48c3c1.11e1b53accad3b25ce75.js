7adb5bf0a62e24d4a15c24c375eaba48f346a3b6
edidiongumana <official.edidiongumana@gmail.com>
2021-03-24 15:11:02 +0100

    Update and first commit

---
+++ _next/static/chunks/1a48c3c1.11e1b53accad3b25ce75.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[8],{qtta:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var c=a("Lnxd");function i(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M4 11H16V13H4zM4 6H20V8H4zM4 18L11 18 11.235 18 11.235 16 11 16 4 16z"}}]})(t)}}}]);
